c23352a787eb6666418f79c0c26ba50782659b4d
Mohit Kala <73868188+mohitkalajain@users.noreply.github.com>
2023-02-16 18:13:53 +0530

    init

---
+++ Controllers/OpenAIController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace ChatGptExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAIController : ControllerBase
+    {
+        [HttpPost]
+        [Route("getanswer")]
+        public IActionResult GetResult([FromBody] string prompt)
+        {
+            //your OpenAI API key
+            string apiKey = "sk-7x1sndXUM6gsR4oTXTVvT3BlbkXXXXXXXXXXXXXXXXXXXX";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = prompt;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            completion.MaxTokens = 4000;
+            var result = openai.Completions.CreateCompletionAsync(completion);
+            if (result != null)
+            {
+                foreach (var item in result.Result.Completions)
+                {
+                    answer = item.Text;
+                }
+                return Ok(answer);
+            }
+            else
+            {
+                return BadRequest("Not found");
+            }
+        }
+    }
+}
